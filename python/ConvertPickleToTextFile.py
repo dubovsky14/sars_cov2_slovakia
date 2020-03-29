711304f1eea0e413735cba7bc608005d6114bbbf
Michal Dubovsky <michal.dubovsky@cern.ch>
2020-03-29 12:07:01 +0200

    Ading python script for pickle -> txt file conversion

---
+++ python/ConvertPickleToTextFile.py
@@ -0,0 +1,19 @@
+import sys
+import pickle
+
+address_input_file = sys.argv[1]
+address_output_file = sys.argv[2]
+
+matrix = pickle.load( open(address_input_file, "rb" ))
+
+
+n_municip = len(matrix)
+output_file = open(address_output_file, "w")
+
+i=0
+for i in range(n_municip):
+    for j in range(n_municip):
+        output_file.write(str(int(matrix[i][j])) + " ; ")
+    output_file.write("\n")
+
+output_file.close()
